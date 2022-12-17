b164d7adc0fcc2e44518ca87f36e730ad20293a8
Dmytro Shcherbak <developerD76@ukr.net>
2022-12-17 16:11:15 +0100

    Recursion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tab = new int[]{1,5,2,3,9,4,2};
+        //printTab(bobbleSort(tab));
+        //printTab(insertionSort(tab));
+        System.out.println(silnia(26));
+    }
+
+    private static void printTab(int[] tab){
+        for(int elem: tab){
+            System.out.print(elem+" ");
+        }
+    }
+
+    private static int[] bobbleSort(int[] tab){
+       for(int i=0; i<tab.length-1; i++){
+           boolean swapped = false;
+           for(int j=0; j< tab.length-1-i; j++){
+               if(tab[j] > tab[j+1]){
+                   swapped = true;
+                   int temp = tab[j+1];
+                   tab[j+1] = tab[j];
+                   tab[j] = temp;
+               }
+           }
+           if(!swapped) return tab;
+       }
+        return  tab;
+    }
+
+    private static int[] insertionSort(int[] tab){
+        for(int i=1; i<tab.length; i++){
+            int temp = tab[i];
+            int j;
+            for(j=i-1; j>=0 && tab[j] > temp; j--){
+                tab[j+1] = tab[j];
+            }
+            tab[j+1] = temp;
+
+        }
+
+        return tab;
+    }
+
+    private static long silnia(int n){
+        if(n==0) return 1L;
+        return silnia(n-1)*n;
+    }
+
+    private static long fibo(int n){
+        if(n <= 2) return 1;
+        return fibo(n-2)+fibo(n-1);
     }
 }
